1632965b92348b76f8df49ef2e4318ea41cf8d8e
Janani NA <janani2285@gmail.com>
2020-08-01 22:22:34 -0700

    html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const addHtmlRoutes = require("./routes/html-routes.js");
 
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
@@ -16,8 +17,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("*", (req,res) => {
+addHtmlRoutes(app);
+/* app.get("*", (req,res) => {
     res.send("<h1> HELLO, WORLD</h1>")
-});
+}); */
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
