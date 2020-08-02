580432878b7e6d7f1e209323904606a3cd416829
Janani NA <janani2285@gmail.com>
2020-08-01 21:55:48 -0700

    mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 const app = express();
 
 app.get("*", (req,res) => {
