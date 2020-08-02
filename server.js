83a4b57f9bdbd59cc3a7e59d2ce9dfc010dc6785
Janani NA <janani2285@gmail.com>
2020-08-01 22:11:02 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("*", (req,res) => {
     res.send("<h1> HELLO, WORLD</h1>")
